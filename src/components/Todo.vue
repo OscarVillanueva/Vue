6e7a6d7cf8dc15af7d1567c400cf7c06ac66fbce
OscarVillanueva <oscar.villanueva.prieto@gmail.com>
2020-04-27 13:11:28 -0500

    Vue and axios init

---
+++ src/components/Todo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <table width = "80%" border = "1" style="margin: 1rem auto"> 
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Task</th>
+                    <th>Completed</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr 
+                    v-for="todo in todos" 
+                    :key = "todo.id"
+                >
+
+                    <td>
+                        {{todo.id}}
+                    </td>
+                    <td>
+                        {{todo.title}}
+                    </td>
+                    <td>
+                        {{todo.completed}}
+                    </td>
+                    
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default ({
+    data() {
+        return {
+            todos: []
+        }
+    },
+    async mounted () {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos")
+        this.todos = data
+    }
+})
+</script>
